3021fd689efa4d1e4f7e187d005df0a11de114fb
Daniel Morais <danielgmoraisg@gmail.com>
2023-08-06 10:46:25 -0300

    ✨ Command to add stock to user's portifolio

---
+++ app/services/add_stock_to_portifolio.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class AddStockToPortifolio
+  prepend SimpleCommand
+
+  def initialize(user_id:, stock_id:)
+    @user_id = user_id
+    @stock_id = stock_id
+  end
+
+  def call
+    portfolio = User.find(@user_id).portfolio << @stock_id
+    User.update(@user_id, portfolio:)
+  end
+end
---
+++ spec/services/add_stock_to_portifolio_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AddStockToPortifolio do
+  subject(:add_stock_to_portifolio) { described_class.call(user_id:, stock_id:) }
+
+  let(:stock_id) { create(:stock).id }
+  let(:user) { create(:user) }
+  let(:user_id) { user.id }
+
+  context 'when succeding' do
+    it 'succeeds' do
+      expect(add_stock_to_portifolio).to be_success
+      expect(User.find(user_id).portfolio).to eq [stock_id]
+    end
+
+    context 'when user already has stocks' do
+      let!(:existing_stock_id) { create(:stock, :from_user, user:).id }
+
+      it 'appends the value to the end of the array' do
+        expect(add_stock_to_portifolio).to be_success
+        expect(User.find(user_id).portfolio).to eq [existing_stock_id, stock_id]
+      end
+    end
+  end
+end
