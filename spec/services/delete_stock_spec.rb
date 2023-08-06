c1815d260bdc2489a9db4b503a39cd6693504c4d
Daniel Morais <danielgmoraisg@gmail.com>
2023-08-06 09:14:22 -0300

    🧪 Create spec for stock deletion

---
+++ spec/services/delete_stock_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UpdateStock do
+  subject(:delete_stock) { described_class.call(id:) }
+
+  context 'when stock exists' do
+    let(:id) { create(:stock, name:).id }
+
+    before do
+      delete_stock
+    end
+
+    it 'deletes the stock' do
+      expect(Stock.find_by_id(id)).to be_nil
+    end
+  end
+  context 'when stock does not exist' do
+    let(:id) { 1_134 }
+    it 'does not raises error' do
+      expect(delete_stock).to be_success
+    end
+  end
+end
