1252235649cba6051ffad9ea94cd86a6fde7a810
Daniel Morais <danielgmoraisg@gmail.com>
2023-08-20 09:01:37 -0300

    🚨 Fix rubocop

--- app/services/stocks/delete_stock.rb
+++ app/services/stocks/delete_stock.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module Stocks
   class DeleteStock
     prepend SimpleCommand
--- app/services/stocks/search_stock_by_attribute.rb
+++ app/services/stocks/search_stock_by_attribute.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module Stocks
   class SearchStockByAttribute
     prepend SimpleCommand
